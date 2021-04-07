e8963a92b7141c4721b184835725261b0f06d1d5
somu-a <someswar.amujala@gmail.com>
2021-04-06 23:22:41 -0700

    fix unicode.py

--- containers/unicode.py
+++ containers/unicode.py
@@ -115,4 +115,4 @@ class NormalizedIter:
             raise StopIteration
         else:
             self.i += 1
-            return self.text[self.ind - 1]
+            return self.text[self.i - 1]
